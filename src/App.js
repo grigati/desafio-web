e4a32dfc90108fec826f489633a88996ca843b2a
Esther <esther.bsb@gmail.com>
2019-05-18 09:44:33 -0300

    Cria lógica de login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import PaginaInicial from './PaginaInicial';
 import Login from './Login';
 import Cadastro from './Cadastro';
+import { getUsuarioAutenticado } from './APIUtils';
 
-function App() {
-  return (
-    <Router>
-      <Link to="/entrar">Entrar</Link>
-      <Link to="/cadastrar">Cadastrar</Link>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+      isLoading: false
+    }
 
+    this.handleEntrar = this.handleEntrar.bind(this);
+    this.handleSair = this.handleSair.bind(this);
+    this.carregarUsuario = this.carregarUsuario.bind(this); 
+  }
+
+  carregarUsuario() {
+    getUsuarioAutenticado()
+    .then(response => {
+      this.setState({
+        currentUser: response
+      });
+    }).catch(error => {
+      this.setState({
+        currentUser: null
+      });  
+    });
+  }
+
+  componentDidMount() {
+    this.carregarUsuario();
+  }
+
+  handleSair() {
+    localStorage.removeItem('ACCESS_TOKEN');
+
+    this.setState({
+      currentUser: null
+    });
+  }
+
+  handleEntrar() {
+    this.carregarUsuario();
+  }
+
+  render() {
+    return (
       <div>
-        <Route exact path="/" component={PaginaInicial} />
-        <Route path="/entrar" component={Login} />
-        <Route path="/cadastrar" component={Cadastro} />
+        <Link to="/entrar">Entrar</Link>
+        <Link to="/cadastrar">Cadastrar</Link>
+
+        <div>
+          <Switch>  
+            <Route exact path="/" component={PaginaInicial} />
+            <Route path="/entrar" 
+                  render={(props) => <Login onEntrarSucesso={this.handleEntrar} {...props} /> } />
+            <Route path="/cadastrar" component={Cadastro} />
+          </Switch>
+        </div>
       </div>
-    </Router>
-  );
+    );
+  }
 }
 
 export default App;

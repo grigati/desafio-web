ca06708552f6294cca17c914c02c7b7262684197
Esther <esther.bsb@gmail.com>
2019-05-18 09:44:17 -0300

    Adiciona formulário de login

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,62 @@
 import React from 'react';
+import { solicitarLogin } from './APIUtils';
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.cpf = React.createRef();
+        this.senha = React.createRef();
+
+        this.state = {
+            erro: null
+        }
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const login = {
+            cpf: this.cpf.current.value,
+            senha: this.senha.current.value
+        }
+
+        solicitarLogin(login)
+        .then(response => {
+            this.props.onEntrarSucesso();
+        })
+        .catch(error => {
+            this.setState({
+                erro: error
+            });
+        });
+      }
+
     render() {
         return (
-              <div>
-                Login
-              </div>
-          );
+            <div className="mx-auto" style={{maxWidth:"700px"}}>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputcpf1">CPF</label>
+                        <input type="text" className="form-control" id="exampleInputcpf1" aria-describedby="cpfHelp" ref={this.cpf} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Senha</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" ref={this.senha} />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+
+                { this.state.erro ? 
+                    <div className="alert alert-danger" role="alert">
+                        Houve um erro ao fazer o login.
+                        { this.state.erro }
+                    </div>
+                  : null
+                }
+            </div>
+        );
     }
 }
 

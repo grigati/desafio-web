33681899381c1312c106fe20fefdfcee29ede2f6
Esther <esther.bsb@gmail.com>
2019-05-18 10:34:37 -0300

    Adiciona lista de usuarios

--- src/PaginaInicial.js
+++ src/PaginaInicial.js
@@ -1,12 +1,99 @@
 import React from 'react';
+import  { Redirect } from 'react-router-dom';
+import { getUsuarios } from './APIUtils';
 
 class PaginaInicial extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            usuarios: null
+        }
+    }
+
+    componentDidMount() {
+        getUsuarios()
+        .then(response => {
+            this.setState({usuarios: response})
+        })
+        .catch(erro => {
+            console.error(erro);
+        });
+    }
+
     render() {
+        if (this.props.usuarioAutenticado) {
+            if (this.state.usuarios) {
+                return (
+                    <table className="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">id</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">CPF</th>
+                            <th scope="col">CEP</th>
+                            <th scope="col">Logradouro</th>
+                            <th scope="col">Complemento</th>
+                            <th scope="col">Bairro</th>
+                            <th scope="col">Cidade</th>
+                            <th scope="col">UF</th>
+                            <th scope="col">Permissões</th>
+                            <th scope="col">Telefones</th>
+                            { (this.props.usuarioAutenticado.roles[0].name === "ROLE_ADMIN") ?
+                                <th scope="col">Ações</th>
+                                : null
+                            }
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.usuarios.map(usuario => {
+                                return (
+                                    <tr key={usuario.id}>
+                                        <th scope="row">{usuario.id}</th>
+                                        <td>{usuario.nome}</td>
+                                        <td>{usuario.cpf}</td>
+                                        <td>{usuario.cep}</td>
+                                        <td>{usuario.logradouro}</td>
+                                        <td>{usuario.complemento}</td>
+                                        <td>{usuario.bairro}</td>
+                                        <td>{usuario.cidade}</td>
+                                        <td>{usuario.uf}</td>
+                                        <td>{usuario.roles[0].name}</td>
+                                        <td>{usuario.telefones ?
+                                            usuario.telefones.map(telefone => {
+                                                return(
+                                                    <span key={telefone.id} className="d-block">
+                                                        {telefone.telefone} {telefone.tipo}
+                                                    </span>
+                                                );
+                                            })
+                                            : <span>Nenhum cadastrado</span>
+                                            }
+                                        </td>
+                                        { (this.props.usuarioAutenticado.roles[0].name === "ROLE_ADMIN") ?
+                                            <td>
+                                                <button className="btn btn-sm btn-primary px-2 pb-1">Editar</button>
+                                                <button className="btn btn-sm btn-primary px-2 pb-1">Deletar</button>
+                                            </td>
+                                            : null
+                                        }
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                );
+            } else {
+                return (
+                    <div>
+                        Nenhum usuário cadastrado
+                    </div>
+                );
+            }
+        }
+            
         return (
-              <div>
-                Página Inicial
-              </div>
-          );
+              <Redirect to="/entrar" />
+        );
     }
 }
 

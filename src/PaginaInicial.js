b7f251e850c84e09efb1da539454a293754d84b0
Esther <esther.bsb@gmail.com>
2019-05-19 19:00:12 -0300

    Adiciona máscara ao telefone na página inicial

--- src/PaginaInicial.js
+++ src/PaginaInicial.js
@@ -79,8 +79,12 @@ class PaginaInicial extends React.Component {
                                         <td>{usuario.telefones ?
                                             usuario.telefones.map(telefone => {
                                                 return(
-                                                    <span key={telefone.id} className="d-block">
-                                                        {telefone.telefone + " (" + telefone.tipo + ")"}
+                                                    <span key={telefone.id} className="d-block mb-3">
+                                                        { telefone.tipo + ": " +
+                                                         ((telefone.tipo === "comercial" || telefone.tipo === "residencial") ?
+                                                            telefone.telefone.replace(/(\d{2})(\d{4})(\d{4})/g, "($1) $2-$3")
+                                                            : telefone.telefone.replace(/(\d{2})(\d{5})(\d{4})/g, "($1) $2-$3"))
+                                                        }
                                                     </span>
                                                 );
                                             })
